b5bc0e270c68ac06f823f9665632ecbce382018a
Hung Nguyen Manh <hungnm@Hungs-MacBook-Pro.local>
2023-11-10 22:16:36 +0700

    f

--- publishes/lf/box/create.blade.php
+++ publishes/lf/box/create.blade.php
@@ -7,6 +7,9 @@
             <span class="btn" wire:click="store">Create</span>
             <span class="btn-default" wire:click="resetForm">Refresh</span>
         </div>
-        <span class="btn-default" @click="$dispatch('resetModal')">Cancel</span>
+        <div class="flex-none flex space-x-4">
+            <x-lf.form.toggle name="create" title="Kept Form" class="w-auto mt-2" />
+            <span class="btn-default" @click="$dispatch('resetModal')">Cancel</span>
+        </div>
     </div>
 </x-lf.card>
